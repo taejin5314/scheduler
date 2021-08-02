5b00c34663816e12082409e0e270a0993d3f5ce3
Taejin Jung <taejin5314@gmail.com>
2021-08-02 15:18:40 -0400

    update DayList component

---
+++ src/components/DayList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DayListItem from 'components/DayListItem';
+
+export default function DayList(props) {
+  return (
+    <ul>
+      {props.days.map((day) => (
+        <DayListItem name={day.name} spots={day.spots} selected={day.name === props.day} setDay={props.setDay} />
+      ))}
+    </ul>
+  );
+}
