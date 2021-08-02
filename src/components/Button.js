62c28053b48ecf1c1f0e95ea29ded1bc1800b76a
Taejin Jung <taejin5314@gmail.com>
2021-08-02 14:34:57 -0400

    react storybook and jest

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,14 @@
 import React from "react";
+import classNames from 'classnames';
 
-import "components/Button.scss";
+import 'components/Button.scss';
 
 export default function Button(props) {
-   return <></>;
+  const buttonClass = classNames('button', { 'button--confirm': props.confirm }, { 'button--danger': props.danger });
+
+  return (
+    <button disabled={props.disabled} onClick={props.onClick} className={buttonClass}>
+      {props.children}
+    </button>
+  );
 }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import 'components/DayListItem.scss';
+
+export default function DayListItem(props) {
+  const listClass = classNames({ 'day-list__item': true, 'day-list__item--selected': props.selected, 'day-list__item--full': props.spots === 0 });
+  return (
+    <li onClick={() => props.setDay(props.name)} className={listClass}>
+      <h2 className="text--regular"> {props.name} </h2> <h3 className="text--light"> {props.spots} spots remaining </h3>{' '}
+    </li>
+  );
+}
