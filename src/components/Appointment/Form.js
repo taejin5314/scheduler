130130eb4b9c4c4976299c77d91183f6b1ebba13
Taejin Jung <taejin5314@gmail.com>
2021-08-09 16:29:22 -0400

    Add more test for Form.test.js to raise coverage

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -28,13 +28,14 @@ export default function Form(props) {
       return;
     }
 
+    setError('');
     props.onSave(name, interviewer);
   };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
